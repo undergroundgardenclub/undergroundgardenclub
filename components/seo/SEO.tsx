d21b333a89abf63d4cf0c42896eefe3f0f5e5163
Mark <x@markthemark.com>
2023-04-19 12:29:55 -0400

    ⚘

--- components/seo/SEO.tsx
+++ components/seo/SEO.tsx
@@ -10,8 +10,8 @@ type TSEOProps = {
 
 export const SEO: React.FC<TSEOProps> = (props) => {
   const {
-    title = "⚘ UNDERGROUND ⚘ GARDEN ⚘ CLUB ⚘",
-    description = "LAB, GARDEN, PLANET AS SANCTUARY AND PLAYGROUND",
+    title = "UNDERGROUND GARDEN CLUB",
+    description = "Lab, Garden, Planet as Sanctuary and Playground",
     image = "/banner2.png",
     url = "https://undergroundgarden.club",
   } = props;
@@ -32,7 +32,7 @@ export const SEO: React.FC<TSEOProps> = (props) => {
       <meta property="og:image" content={image} />
       <meta property="og:url" content={url} />
       <meta property="og:type" content="website" />
-      <meta property="og:site_name" content="⚘ UNDERGROUND ⚘ GARDEN ⚘ CLUB ⚘" />
+      <meta property="og:site_name" content={title} />
 
       {/* Twitter */}
       <meta name="twitter:card" content="summary_large_image" />
