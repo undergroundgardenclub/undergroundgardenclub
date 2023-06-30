f59aa215b44f50735c85bfd9509b2431f0e01360
Mark <x@markthemark.com>
2023-06-30 00:37:35 -0400

    ⚘

--- next.config.js
+++ next.config.js
@@ -6,9 +6,22 @@ const nextConfig = {
     styledComponents: true,
   },
   // Prefix NEXT_PUBLIC_ exposes envs by default, but we're pulling from secrets manager
-  env: {},
+  env: {
+    SUPABASE_URL: process.env.DATABASE_SUPABASE_URL,
+    SUPABASE_ANON_KEY: process.env.DATABASE_SUPABASE_PUBLIC_KEY,
+  },
   reactStrictMode: true,
   trailingSlash: false,
+  redirects: async () => {
+    return [
+      {
+        source: "/privacy-policy",
+        destination:
+          "https://undergroundgardenclub.notion.site/Privacy-Policy-4e2d7108200d4a4cbde30c1a34229fa7?pvs=4",
+        permanent: false,
+      },
+    ];
+  },
 };
 
 module.exports = nextConfig;
