bdfb5c15c350a2fd5aaaacf78104bfe1d5e5bdfa
Mark <x@markthemark.com>
2023-07-02 19:57:54 -0400

    ⚘: home header action/nav

--- pages/index.tsx
+++ pages/index.tsx
@@ -4,22 +4,27 @@ import React from "react";
 import styled from "styled-components";
 import { SEO } from "../components/seo/SEO";
 import { QuestTree } from "../components/quests/QuestTree";
-import { Header } from "../components/layout/Header";
+import { HeaderActions } from "../components/layout/HeaderActions";
 import { TQuest } from "../components/quests/types";
 import { QuestList } from "../components/quests/QuestList";
+import { ProjectList } from "../components/projects/ProjectList";
+import { ugcTheme } from "../components/styled/theme";
+import { HeaderNav } from "../components/layout/HeaderNav";
 
 export default function HomePage({ quests }: { quests: TQuest[] }) {
   // RENDER
   return (
     <>
       <SEO />
-      {/* NAV */}
-      <Header />
-      {/* VIEW */}
+      <HeaderNav />
       <StyledHomePage>
         <main>
           <QuestTree quests={quests} />
         </main>
+        <aside>
+          <HeaderActions invert />
+          <ProjectList />
+        </aside>
       </StyledHomePage>
     </>
   );
@@ -29,12 +34,29 @@ const StyledHomePage = styled.div`
   min-height: 100vh;
   margin: 0;
   padding: 0;
+  position: absolute;
+  top: 0;
+  right: 0;
+  left: 0;
+  bottom: 0;
+  display: flex;
   main {
-    width: 100%;
-    max-width: 400px;
-    min-height: 120px;
-    margin: 0 auto;
+    margin: 0;
     z-index: 1;
+    flex-grow: 1;
+    height: 100%;
+  }
+  aside {
+    width: 420px;
+    padding: 0.75em;
+    background: ${ugcTheme.colors.blue[100]};
+    overflow: hidden;
+    & > header {
+      margin-bottom: 0.5em;
+    }
+    @media (max-width: 45em) {
+      display: none;
+    }
   }
 `;
 
--- pages/projects/index.tsx
+++ pages/projects/index.tsx
@@ -1,24 +1,19 @@
 import React from "react";
-import { useProjects } from "../../components/projects/useProjects";
 import styled from "styled-components";
 import { ugcTheme } from "../../components/styled/theme";
-import { Header } from "../../components/layout/Header";
-import { ProjectPanel } from "../../components/projects/ProjectPanel";
-import { ProjectCreatorPanel } from "../../components/projects/ProjectCreatorPanel";
+import { HeaderActions } from "../../components/layout/HeaderActions";
+import { ProjectCreatorButton } from "../../components/projects/ProjectCreatorButton";
+import { ProjectList } from "../../components/projects/ProjectList";
 
 export default function ProjectsPage() {
-  const { data: projects } = useProjects();
   // RENDER
   return (
     <>
-      <Header invert />
       <StyledProjectsPage>
-        <div className="projects-list">
-          {projects?.map((project) => (
-            <ProjectPanel key={project.id} project={project} />
-          ))}
+        <HeaderActions invert />
+        <div className="projects">
+          <ProjectList />
         </div>
-        <ProjectCreatorPanel />
       </StyledProjectsPage>
     </>
   );
@@ -31,16 +26,12 @@ const StyledProjectsPage = styled.div`
   min-width: 100vw;
   height: 100vh;
   position: fixed;
-  & > div.projects-list {
-    display: flex;
-    flex-direction: column;
+  overflow-y: scroll;
+  & > * {
     max-width: 600px;
-    margin: 2em auto;
-    &:first-of-type {
-      margin-top: 5em;
-    }
-    & > div {
-      margin: 0.5em 0;
-    }
+    margin: 0 auto 1em;
+  }
+  & > .projects {
+    padding: 0.5em;
   }
 `;
