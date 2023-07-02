18d432c3947fba6c0a10e2f56aa539169d0146b0
Mark <x@markthemark.com>
2023-07-02 15:02:06 -0400

    ⚘: theme + styled button variants

---
+++ components/styled/StyledButton.tsx
@@ -0,0 +1,39 @@
+import styled from "styled-components";
+import { ugcTheme } from "./theme";
+
+type TStyledButtonVariant = "green" | "blue" | "white" | "transparent";
+
+export const StyledButton = styled.button<{ variant?: TStyledButtonVariant }>`
+  pointer-events: all;
+  text-transform: uppercase;
+  background: ${(props) => {
+    if (props.variant === "green") return ugcTheme.colors.green[500];
+    if (props.variant === "blue") return ugcTheme.colors.blue[500];
+    return "transparent";
+  }};
+  color: ${(props) => {
+    if (props.variant === "green") return ugcTheme.colors.blue[500];
+    if (props.variant === "blue") return ugcTheme.colors.white[900];
+    if (props.variant === "white") return ugcTheme.colors.white[900];
+    return "black";
+  }};
+  border: ${(props) => {
+    if (props.variant === "green") return "none";
+    if (props.variant === "blue") return "none";
+    if (props.variant === "white") return "solid 2px white";
+    return "solid 2px black";
+  }};
+  font-family: ${ugcTheme.fonts.display};
+  padding: 8px 12px 6px;
+  font-size: 13px;
+  cursor: pointer;
+  // transform: skew(-5deg, 0);
+  height: 32px;
+`;
+
+export const StyledButtonAvatar = styled(StyledButton)`
+  padding: 0;
+  img {
+    height: 100%;
+  }
+`;
---
+++ components/styled/theme.tsx
@@ -0,0 +1,21 @@
+export const ugcTheme = {
+  colors: {
+    blue: {
+      100: "#000088",
+      500: "#0000FF",
+      700: "#00BBFF",
+    },
+    green: {
+      500: "#1f1",
+    },
+    white: {
+      500: "#f0f0f0",
+      700: "#fafafa",
+      900: "#ffffff",
+    },
+  },
+  fonts: {
+    display: "'Stonewall 50'",
+    readability: "'CoFo Rax'",
+  },
+};
