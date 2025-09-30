1d819bc0388cbf7d52867863889eebfcb6767e36
Mark <x@markthemark.com>
2025-09-29 23:36:48 -0700

    ⚘: deploy

--- next.config.js
+++ next.config.js
@@ -5,11 +5,13 @@ const nextConfig = {
     // Enables the styled-components SWC transform
     styledComponents: true,
   },
+  distDir: "dist",
   // Prefix NEXT_PUBLIC_ exposes envs by default, but we're pulling from secrets manager
   env: {
     SUPABASE_URL: process.env.DATABASE_SUPABASE_URL,
     SUPABASE_ANON_KEY: process.env.DATABASE_SUPABASE_PUBLIC_KEY,
   },
+  output: "export",
   reactStrictMode: true,
   trailingSlash: false,
   redirects: async () => {
